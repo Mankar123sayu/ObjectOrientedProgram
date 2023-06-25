7e17013aa6ec1923e63a364666045b0fbecde96a
Sayunkta Mankar <sayumankar77@gmail.com>
2023-06-25 16:12:11 +0530

    Add

---
+++ ObjectOrientedProgram/InventoryDetails.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectOrientedProgram
+{
+    public class InventoryDetails
+    {
+        public List<TypeOfRice> typeOfRice;
+        public List<TypeOfPulses> typeOfPulses;
+        public List<TypeOfWheats> typeOfWheats;
+    }
+    public class TypeOfRice
+    {
+
+        public string name;
+        public int weight;
+        public int price;
+    }
+    public class TypeOfPulses
+    {
+        public string name;
+        public int weight;
+        public int price;
+    }
+    public class TypeOfWheats
+    {
+        public string name;
+        public int weight;
+        public int price;
+    }
+
+}
+    
+
---
+++ ObjectOrientedProgram/ReadTheData.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectOrientedProgram
+{
+    public class ReadTheData
+    {
+        public InventoryDetails Read(string path)
+        {
+            using (StreamReader file = new StreamReader(path))
+            {
+                try
+                {
+                    string json = file.ReadToEnd();
+                    return JsonConvert.DeserializeObject<InventoryDetails>(json);
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    return null;
+                }
+            }
+        }
+    }
+}
+    
+
